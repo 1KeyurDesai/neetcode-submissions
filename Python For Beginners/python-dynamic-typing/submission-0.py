5949aa2598ef660289b2537c37e242ca21a62dc5
Keyur Desai <167679306+1KeyurDesai@users.noreply.github.com>
2026-08-30 00:54:16 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.5
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "Keyur"
+print(type(variable))
+variable = [10, 85, 100]
+print(type(variable))
